4df43353a5535ca751106abd9e4f89863c55edae
Deepak Sai Pendyala <76196490+deepaksaipendyala@users.noreply.github.com>
2021-07-18 01:59:22 +0530

    Added more features

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ api = tweepy.API(auth)
 
 user = api.me()
 
+#print about yourself
+
+print (user.name) #prints your name.
+print (user.screen_name)
+print (user.followers_count)
+
+#print tweets in your timeline
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
+
 def limit_handler(cursor):
   try:
     while True:
@@ -23,25 +34,15 @@ for follower in limit_handler(tweepy.Cursor(api.followers).items()):
   print(follower.name)
   follower.follow()
 
+# Be a narcisist and love your own tweets. or retweet anything with a keyword!
+search = "deepak"
+numberOfTweets = 2
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# print(user.name)
-# print(user.followers_count)
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#     print(tweet.text)
+for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+    try:
+        tweet.favorite()
+        print('Retweeted the tweet')
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
